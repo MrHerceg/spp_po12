cfbf39af43910b7665150ae738494932a51584ea
vladp <vladpashkevich88@gmail.com>
2026-03-03 15:11:22 +0300

    feat: add lab1

--- reports/Pashkevich_V_V/LAB01/src/LAB0102.py
+++ reports/Pashkevich_V_V/LAB01/src/LAB0102.py
@@ -24,7 +24,7 @@ def climb_stairs():
         prev1 = 2  # Способы для (n-1)
         current = 0
 
-        for i in range(3, n + 1):
+        for _ in range(3, n + 1):
             current = prev1 + prev2
             prev2 = prev1
             prev1 = current
